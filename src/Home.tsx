f5169053c34a8236a3d5645ac064a01628580271
epelix <90451715+eemeliHuotari@users.noreply.github.com>
2025-05-21 21:42:59 +0300

    Update Home.tsx

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,8 +1,8 @@
 const Home: React.FC = () => {
   return (
     <div className="p-6 text-center">
-      <h1 className="text-3xl font-bold">Welcome to the Burgir App 💖</h1>
-      <p className="mt-2">Use the menu to navigate</p>
+      <h1 className="text-3xl font-bold">MMMMMMoiiii 💖</h1>
+      <p className="mt-2">JOS LUET TÄN OOOOOOT MAAAAAILMAN KAUNEIN JA IHANIN TYTTÖYSTÄVÄ JA SUN (TYHMÄ) tuleva aviomies sai tän frontin joskus aikaseks </p>
     </div>
   );
 };
