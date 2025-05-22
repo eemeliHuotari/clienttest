9814e3238e2ff50ad50dfe7acfe00ef9c3960ece
epelix <90451715+eemeliHuotari@users.noreply.github.com>
2025-05-22 21:18:18 +0300

    Update fetchAllPaginated.ts

--- src/fetchAllPaginated.ts
+++ src/fetchAllPaginated.ts
@@ -2,16 +2,22 @@ import axios from 'axios';
 import { API_BASE } from './config';
 
 export async function fetchAllPaginated<T>(endpoint: string): Promise<T[]> {
-    // Fetch all paginated data from the given endpoint
-    // and return the results as an array of generic type T.
   let results: T[] = [];
-  let url = `${endpoint}`;
+  let url = endpoint.startsWith('/api') ? endpoint : `/api${endpoint}`;
 
   while (url) {
     const res = await axios.get(`${API_BASE}${url}`);
     const data = res.data;
+
     results = [...results, ...(data.results || data)];
-    url = data.next ? data.next.replace('https://helionmunvaimo.2.rahtiapp.fi', '') : '';
+
+    // Remove full base URL from `next` to avoid duplicated hostnames
+    if (data.next) {
+      const nextUrl = new URL(data.next);
+      url = nextUrl.pathname + nextUrl.search;
+    } else {
+      url = '';
+    }
   }
 
   return results;
